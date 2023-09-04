46fe42f2a0c468dcb5c05c4a3cd9c5f4aecb53dc
Chris <krzy.driwa@gmail.com>
2023-09-04 23:09:33 +0200

    Add pseudocode for completing the game

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+    //return randomly chosen symbol from: rock, paper, scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+    //make playerSelection case insensitive
+    //compare chosen symbols
+    //return a string declaring a winner, eg. You lose! Paper beats Rock
+}
+
+function game() {
+    //play a 5 round game
+    //keep score
+    //report winner/loser at the end
+}
