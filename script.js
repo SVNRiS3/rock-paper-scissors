d326621a810d1244241e694d283eb593c3b28581
Chris <krzy.driwa@gmail.com>
2023-09-05 00:05:11 +0200

    Create game

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+let roundNumber = 0, playerScore = 0, computerScore = 0;
+
+
 function getComputerChoice() {
-    //return randomly chosen symbol from: rock, paper, scissors
+    let chosenSymbol = Math.floor(Math.random() * 3) + 1;
+    return chosenSymbol === 1 ? "Rock" : chosenSymbol === 2 ? "Paper" : "Scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
-    //make playerSelection case insensitive
-    //compare chosen symbols
-    //return a string declaring a winner, eg. You lose! Paper beats Rock
+    playerSelectionCapitalized = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let message = '';
+    if (!['Rock', 'Paper', 'Scissors'].includes(playerSelectionCapitalized)) {
+        console.log('Please choose the right symbol!');
+        return;
+    } else if (playerSelectionCapitalized === computerSelection) {
+        message = "It's a draw";
+    } else if (playerSelectionCapitalized === "Rock" && computerSelection === "Paper" || playerSelectionCapitalized === "Paper" && computerSelection === "Scissors" || playerSelectionCapitalized === "Scissors" && computerSelection === "Rock") {
+        message = `You lose! ${computerSelection} beats ${playerSelectionCapitalized}`;
+        computerScore++;
+    } else {
+        message = `You win! ${playerSelectionCapitalized} beats ${computerSelection}`;
+        playerScore++;
+    }
+    roundNumber++;
+    return message;
+}
+
+function checkScore(playerScore, computerScore) {
+    return playerScore === computerScore ? "It's a draw, there's no winner!" : playerScore > computerScore ? "You won, congratulations!" : "You lost :(";
 }
 
 function game() {
-    //play a 5 round game
-    //keep score
-    //report winner/loser at the end
+
+    while (roundNumber < 5) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Please write your symbol: ");
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    console.log(checkScore(playerScore, computerScore));
 }
+
+game();
